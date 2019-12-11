b8202e1871033e58863489a469b3fc61e83dab94
Rita <ritaokonkwo6@gmail.com>
2019-12-11 13:15:54 +0100

    webview et action menu

--- app/src/main/java/com/project/androidlivetrack/Welcome.java
+++ app/src/main/java/com/project/androidlivetrack/Welcome.java
@@ -1,8 +1,14 @@
 package com.project.androidlivetrack;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class Welcome extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_menu) {
+            Intent intent = new Intent(Welcome.this, ActionActivity.class);
+            startActivity(intent);
+        }
+        return true;
     }
 }
