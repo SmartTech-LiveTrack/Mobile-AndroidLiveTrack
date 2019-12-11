e3e666d05df7643706ef89b82452d2c3953a68b8
Adebiyi Emmanuel <smartemma03@gmail.com>
2019-12-11 13:49:47 +0100

    new modifications

---
+++ app/src/main/java/com/project/androidlivetrack/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.project.androidlivetrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        int SPLASH_TIME_OUT = 2200;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
